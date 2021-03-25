ba2b0cdf1b2ab1a0b4e4422feba40d68a245dea9
Shux_DEV <70896657+PonchHKT@users.noreply.github.com>
2021-03-25 12:55:14 +0100

    Update

--- components/login.js
+++ components/login.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ImageBackground, Image, Dimensions, TextInput, TouchableOpacity, Button } from 'react-native';
-import bgImage from '../assets/background.jpg';
-import logo from '../assets/motocrosslogo.png';
+import { StyleSheet, Text, View, ImageBackground, Image, Dimensions, TextInput, TouchableOpacity } from 'react-native';
+import bgImage from '../assets/images/background.jpg';
+import logo from '../assets/images/motocrosslogo.png';
 import Icon from 'react-native-vector-icons/Ionicons'
 
 const { width: WIDTH } = Dimensions.get('window')
@@ -22,7 +22,7 @@ const { width: WIDTH } = Dimensions.get('window')
     }
         navigation.reset({
         index: 0,
-        routes: [{ name: 'todolist' }],
+        routes: [{ name: '' }],
     })
 }
     const changeSecurity = () => {
@@ -37,68 +37,61 @@ const { width: WIDTH } = Dimensions.get('window')
     <ImageBackground source={bgImage} style={styles.backgroundContainer}>
       <View style={styles.logoContainer}>
         <Image source={logo} style={styles.logo}></Image>
-        <Text style={styles.logoText}>AYMENE & PONCH</Text>
+        <Text style={styles.logoText}>OFFROAD BIKE TRIP</Text>
       </View>
       <View style={styles.inputContainer}>
-        <Icon name={'ios-person-outline'} size={28} color={'rgba(255, 255, 255, 0.7)'} 
-        style={styles.inputIcon} />
+        <Icon name={'mail-outline'} size={28} color={'black'} 
+            style={styles.inputIcon} />
         <TextInput
-        style={styles.input}
-        returnKeyType="next"
-        value={email.value}
-        onChangeText={(text) => setEmail({ value: text, error: '' })}
-        error={!!email.error}
-        errorText={email.error}
-        autoCapitalize="none"
-        autoCompleteType="email"
-        textContentType="emailAddress"
-        keyboardType="email-address"
-        placeholder={'Email'}
-        placeholderTextColor={'rgba(255, 255, 255, 0.7)'}
-        underlineColorAndroid='transparent'
-      />
+            style={styles.input}
+            returnKeyType="next"
+            value={email.value}
+            onChangeText={(text) => setEmail({ value: text, error: '' })}
+            error={!!email.error}
+            errorText={email.error}
+            autoCapitalize="none"
+            autoCompleteType="email"
+            textContentType="emailAddress"
+            keyboardType="email-address"
+            placeholder={'Email'}
+            placeholderTextColor={'black'}
+            underlineColorAndroid='transparent'/>
       <Text style={styles.error}>{email.error}</Text>
       </View>
 
       <View style={styles.inputContainer}>
-        <Icon name={'lock-closed-outline'} size={28} color={'rgba(255, 255, 255, 0.7)'} 
-        style={styles.inputIcon} />
+        <Icon name={'lock-closed-outline'} size={28} color={'black'} 
+            style={styles.inputIcon} />
         <TextInput
-        style={styles.input}
-        placeholder={'Password'}
-        returnKeyType="done"
-        value={password.value}
-        onChangeText={(text) => setPassword({ value: text, error: '' })}
-        error={!!password.error}
-        errorText={password.error}
-        placeholderTextColor={'rgba(255, 255, 255, 0.7)'}
-        underlineColorAndroid='transparent'
-        secureTextEntry={security}
-      />
-      
+            style={styles.input}
+            placeholder={'Password'}
+            returnKeyType="done"
+            value={password.value}
+            onChangeText={(text) => setPassword({ value: text, error: '' })}
+            error={!!password.error}
+            errorText={password.error}
+            placeholderTextColor={'black'}
+            underlineColorAndroid='transparent'
+            secureTextEntry={security} />
       <TouchableOpacity style={styles.btnEye}>
-        <Icon onPress={changeSecurity} name={'ios-eye-outline'} size={26} color={'rgba(255, 255, 255, 0.7)'}/>
+        <Icon onPress={changeSecurity} name={'ios-eye-outline'} size={26} color={'black'}/>
       </TouchableOpacity>
       <Text style={styles.error}>{password.error}</Text>
       </View>
+
       <TouchableOpacity 
-      style={styles.btnLogin}
-      onPress={() => navigation.navigate('todolist')}
-      onPress={onLoginPressed}>
-        <Text
-        style={styles.text} >Login</Text>
+            style={styles.btnLogin}
+            onPress={() => navigation.navigate('todolist')}
+            onPress={onLoginPressed}>
+      <Text style={styles.text} >Login</Text>
       </TouchableOpacity>
 
-        <Text
-        style={styles.noAccount}>Don't have an account yet?</Text>
+        <Text style={styles.noAccount}>Don't have an account yet?</Text>
         <TouchableOpacity>
-        <Text onPress={() => navigation.navigate('register')} 
-              style={styles.clickUnderline, styles.noAccount} 
-              fontWeight="bold"> Click here</Text>
+        <Text onPress={() => navigation.navigate('register')} style={styles.clickHere}> Make one!</Text>
       </TouchableOpacity>
       <StatusBar style="auto" />
     </ImageBackground>
-
     ); 
   }
 
@@ -119,29 +112,32 @@ logo: {
   height: 120,
 },
 logoText: {
-  color: 'white',
+  color: 'black',
   fontSize: 20,
   fontWeight: 'bold',
   marginTop: 10,
   opacity: 0.5
 },
 inputContainer: {
-  marginTop: 10,
+  marginTop: 5,
 },
 input: {
   width: WIDTH - 55,
   height: 45,
-  borderRadius: 45,
+  borderRadius: 10,
   fontSize: 16,
   paddingLeft: 45,
-  backgroundColor: 'rgba(0, 0, 0, 0.35)',
-  color: 'rgba(255, 255, 255, 0.7)',
-  marginHorizontal: 25
+  backgroundColor: '#ecf0f1',
+  color: 'black',
+  marginHorizontal: 25,
+  borderWidth: 0.8,
+  borderColor: 'black',
 },
 inputIcon: {
   position: 'absolute',
   top: 8,
   left: 37,
+  zIndex: 10,
 },
 btnEye: {
   position: 'absolute',
@@ -149,10 +145,10 @@ btnEye: {
   right: 37,
 },
 btnLogin: {
-  width: WIDTH - 55,
+  width: WIDTH - 130,
   height: 45,
   borderRadius: 45,    
-  backgroundColor: '#eb401a',
+  backgroundColor: 'rgba(230, 126, 34,1.0)',
   justifyContent: 'center',
   marginTop: 20,
 },
@@ -162,14 +158,17 @@ text: {
   textAlign: 'center',
 },
 noAccount: {
-  color: 'white',
+  color: 'black',
   fontSize: 16,
   textAlign: 'center',
   paddingTop: 20,
 },
-clickUnderline: {
+clickHere: {
   textDecorationLine: 'underline',
   fontWeight: 'bold',
+  color: 'black',
+  fontSize: 16,
+  textAlign: 'center',
 },
 error: {
   alignSelf: 'center',
