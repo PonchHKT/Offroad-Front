982580d9288d98297c39aa969736c378ece021b4
Shux_DEV <70896657+PonchHKT@users.noreply.github.com>
2021-03-25 10:48:40 +0100

    Update

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {login} from './components/login';
+import {register} from './components/register';
+import {forgetpassword} from './components/forgetpassword'
+import {newpassword} from './components/newpassword'
+import {cgu} from './components/cgu.js'
+import {dashboard} from './components/dashboard';
+import {map} from './components/map'
+import {profil} from './components/profil'
+import {localization} from './components/localization'
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator 
+      screenOptions={{
+      headerShown: false
+      }}>
+        <Stack.Screen name="login" component={login} />
+        <Stack.Screen name="register" component={register} />
+        <Stack.Screen name="forgetpassword" component={forgetpassword} />
+        <Stack.Screen name="newpassword" component={newpassword} />
+        <Stack.Screen name="cgu" component={cgu} />
+        <Stack.Screen name="dashboard" component={dashboard} />
+        <Stack.Screen name="map" component={map} />
+        <Stack.Screen name="profil" component={profil} />
+        <Stack.Screen name="localization" component={localization} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
