cc87ae9cda68b8f6eb7ab66632181b3ebdc6b59e
Pornchai Chanaloet <kayziopro@gmail.com>
2021-04-14 13:04:32 +0200

    uPDAte

--- screens/profile/profil.js
+++ screens/profile/profil.js
@@ -9,8 +9,8 @@ const { width: WIDTH, height: HEIGHT } = Dimensions.get('window')
 export function profil({ navigation }) {
   
     return (
-        <View style={{flex: 1}}>
-              <ScrollView>
+        <View style={{flex: 1}} >
+              <ScrollView style={{zIndex: 100}}>
                   <Navbar 
                       key={1}
                       id={1}
@@ -23,6 +23,11 @@ export function profil({ navigation }) {
                       account={true}
                   />
               </ScrollView>
+
+              <View style={styles.backgroundContainer}>
+                
+              </View>
+
 
             <View>
             
@@ -52,7 +57,7 @@ export function profil({ navigation }) {
       
 
             </View>
-            <View style={{flex: 1, alignSelf: 'center', flexDirection: 'row', justifyContent: 'space-around', width: '100%'}}>
+            <View style={{flex: 1, alignSelf: 'center', flexDirection: 'row', justifyContent: 'space-around', width: '99%'}}>
       
                 <CustomButton
                     key={1}
@@ -67,9 +72,8 @@ export function profil({ navigation }) {
                     title={'Options'}
                     width={200}
                 />
-
+                </View>
             </View>
-      </View>
 )};
 
 const styles = StyleSheet.create({
@@ -81,7 +85,8 @@ const styles = StyleSheet.create({
     width: WIDTH / 1.1,
     height: 60,
     alignSelf: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    top: 30,
   },
   front2:{
     color: 'grey',
@@ -89,4 +94,14 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     textAlignVertical: 'center'
   },
+  backgroundContainer: {
+    width: 550,
+    height: 250,
+    position: 'absolute',
+    backgroundColor: 'red',
+    borderColor: 'black',
+    borderWidth: 2,
+    right: 1,
+  },
+
 });
