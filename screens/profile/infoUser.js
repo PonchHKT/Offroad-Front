d6ce799fbfecbf897fd217aa9096a8f6678f46cd
Edofo <Edofo@users.noreply.github.com>
2021-04-15 16:45:54 +0200

    Update profil

--- screens/profile/infoUser.js
+++ screens/profile/infoUser.js
@@ -1,68 +1,90 @@
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-gesture-handler';
-import { StyleSheet, View, Text, TouchableOpacity, ScrollView, Dimensions } from "react-native";
+import { StyleSheet, View, Text, ScrollView, Dimensions } from "react-native";
 import React, { useState } from 'react';
 import Navbar from '../../components/Navbar';
 import CustomInput from '../../components/CustomInput';
 import ModalDropdown from 'react-native-modal-dropdown';
 import CustomButton from '../../components/CustomButton';
+import Separator from '../../components/Separator';
 
 const { width: WIDTH } = Dimensions.get('window')
 
 
-export function infoUser({ navigation }) {
+export function infoUser({ route, navigation }) {
 
-    const [pseudo, setPseudo] = useState({ value: '', error: '' })
-    const [email, setEmail] = useState({ value: '', error: '' })
+    const { userInfos, token } = route.params;
+
+    const [pseudo, setPseudo] = useState({ value: userInfos.pseudo, error: '' })
+    const [email, setEmail] = useState({ value: userInfos.email, error: '' })
     const [password, setPassword] = useState({ value: '', error: '' })
+    const [passwordConfirmation, setPasswordConf] = useState({ value: '', error: '' })
+    const [level, setLevel] = useState({ value: userInfos.level });
+    const [notif, setNotif] = useState({ value: userInfos.notif });
 
-    const [user, setUser] = useState({
-        niveau: '',
-    });
-    function handleGender(text) {
-        setUser({ ...user, gender: text })
+    const submit = async() => {
+        fetch(`https://offroad-app.herokuapp.com/api/users/edit/${userInfos.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+            body: JSON.stringify({
+                pseudo: pseudo.value,
+                email: email.value,
+                password: password.value,
+                passwordConfirmation: passwordConfirmation.value,
+                level: level.value,
+                notif: true,
+            })
+        })
+        .then((response) => response.json())
+        .then((responseData) => {
+            console.log(responseData)
+        })
+        .catch((error) =>{
+            console.error(error);
+        })
     };
 
     return (
-    <ScrollView>
-        <View>
-            <Navbar 
-                key={1}
-                dashboard={false}
-                mapPress={() => navigation.navigate('dashboard')}
-                plus={false}
-                plusPress={() => navigation.navigate('addspot')}
-                like={false}
-                likePress={() => navigation.navigate('')}
-                account={true}
-            />
-        </View>
-        <View style={styles.container}>
-            
-            <Text style={styles.front}>Pseudo :</Text>
-            <CustomInput
-                key={2}
-                placeholder={'Dark Rider'}
-                valeur={pseudo.value}
-                error={!!pseudo.error}
-                errorText={pseudo.error}
-                text={(text) => setPseudo({ value: text, error: '' })}
-            />
+        <ScrollView>
+            <View>
+                <Navbar 
+                    key={1}
+                    dashboard={false}
+                    mapPress={() => navigation.navigate('dashboard')}
+                    plus={false}
+                    plusPress={() => navigation.navigate('addspot')}
+                    like={false}
+                    likePress={() => navigation.navigate('')}
+                    account={true}
+                />
+            </View>
+            <View style={styles.container}>
+                
+                <Text style={styles.front}>Pseudo :</Text>
+                <CustomInput
+                    key={2}
+                    valeur={pseudo.value}
+                    error={!!pseudo.error}
+                    errorText={pseudo.error}
+                    text={(text) => setPseudo({ value: text, error: '' })}
+                />
 
-            <Text style={styles.front}>Adresse email :</Text>
-            <CustomInput
-                key={3}
-                placeholder={'mathilde.tapis@gmail.com'}
-                valeur={email.value}
-                error={!!email.error}
-                errorText={email.error}
-                text={(text) => setEmail({ value: text, error: '' })}
-            />
+                <Text style={styles.front}>Adresse email :</Text>
+                <CustomInput
+                    key={3}
+                    valeur={email.value}
+                    error={!!email.error}
+                    errorText={email.error}
+                    text={(text) => setEmail({ value: text, error: '' })}
+                />
 
-            <Text style={styles.front}>Mot de passe :</Text>
-            <CustomInput
+                <Text style={styles.front}>Mot de passe :</Text>
+                <CustomInput
                     key={4}
-                    placeholder={'Mot de passe'}
                     valeur={password.value}
                     error={!!password.error}
                     errorText={password.error}
@@ -70,20 +92,24 @@ export function infoUser({ navigation }) {
                     pwd={true}
                 />
 
-            <Text style={styles.front}>Niveau :</Text>
-            <View style={styles.picker}>
-            <ModalDropdown
-                  options={ ['Débutant', 'Intermédiaire','Avancé', 'Expert']}
-                  style={{height: 40, width: '100%'}}
-                  dropdownStyle={styles.dropdown}
-                  dropdownTextStyle={{fontSize: 20, width: WIDTH - 70}}
-                  defaultValue={'Appuyer pour choisir'}
-                  textStyle= {{fontSize: 20, marginLeft: 10, marginTop: 5}}
-                />
-                        
-                    </View>
+                <Text style={styles.front}>Niveau :</Text>
+                <View style={styles.picker}>
+                    <ModalDropdown
+                        options={ ['Débutant', 'Intermédiaire','Avancé', 'Expert']}
+                        style={{height: 40, width: '100%'}}
+                        onSelect={(val) => console.log(val)}
+                        dropdownStyle={styles.dropdown}
+                        dropdownTextStyle={{fontSize: 20, width: WIDTH - 70}}
+                        defaultValue={'Appuyer pour choisir'}
+                        textStyle= {{fontSize: 20, marginLeft: 10, marginTop: 5}}
+                    />
+                </View>
 
-            <Text style={styles.front}>Notifications :</Text>
+                <Separator />
+                <Separator />
+                <Separator />
+
+                <Text style={styles.front}>Notifications :</Text>
                 <View style={styles.picker}>
                     <ModalDropdown
                     options={ ['Oui', 'Non']}
@@ -95,29 +121,29 @@ export function infoUser({ navigation }) {
                     />                     
                 </View>
 
-        </View>
-        <View style={styles.container2}>
-            
-        <View style={{flex: 1, alignSelf: 'center', flexDirection: 'row', justifyContent: 'space-around', width: '99%', paddingTop: 10, paddingBottom: 20, marginTop: 20}}>
-      
-        <CustomButton
-                key={1}
-                actionsbtn={() => navigation.navigate('profil')}
-                title={'Retour'}
-                width={200}
-            />
-      
-        <CustomButton
-                key={2}
-                actionsbtn={() => navigation.navigate('')}
-                title={'Modifier'}
-                width={200}
-            />
-      </View>
+            </View>
+            <View style={styles.container2}>
+                
+                <View style={{flex: 1, alignSelf: 'center', flexDirection: 'row', justifyContent: 'space-around', width: '99%', paddingTop: 10, paddingBottom: 20, marginTop: 20}}>
+        
+                    <CustomButton
+                        key={1}
+                        actionsbtn={() => navigation.navigate('profil', { userInfos: userInfos})}
+                        title={'Retour'}
+                        width={200}
+                    />
+        
+                    <CustomButton
+                        key={2}
+                        actionsbtn={() => navigation.navigate('')}
+                        title={'Modifier'}
+                        width={200}
+                    />
+                </View>
 
-        <StatusBar style="auto" hidden={true}/>
-        </View>
-    </ScrollView>
+            </View>
+            <StatusBar style="auto" hidden={true}/>
+        </ScrollView>
 )};
 
 const styles = StyleSheet.create({
@@ -128,35 +154,22 @@ const styles = StyleSheet.create({
         flexDirection:'row',
     },
     front:{
-        marginTop: 30,
+        marginTop: 20,
+        marginBottom: 10,
         color: 'grey',
-        height: 50,
         fontSize: 20,
         marginLeft: '5%'
     },
     picker: {
-            flex: 1,
-            flexDirection: 'row',
-            width: WIDTH - 60,
-            alignSelf: 'center',
-            backgroundColor: "ghostwhite",
-            borderWidth: 1.5,
-            borderColor: "#000000",
-            borderRadius: 20,
-        },
-        buttonArrondi: { backgroundColor: "#E6E6E6",
-        borderRadius: 21,
-        width: '30%',
-        height: 61,
-        marginBottom: '10%',
-        marginLeft: '13%',
-        marginTop: '5%',
-        alignItems: 'center'
-      },
-      btntxt: {
-        color: 'grey',
-        height: 50,
-        fontSize: 20,
-        textAlignVertical: 'center'
-      }
+        flex: 1,
+        flexDirection: 'row',
+        width: WIDTH - 60,
+        alignSelf: 'center',
+        backgroundColor: "ghostwhite",
+        borderWidth: 1.5,
+        borderColor: "#000000",
+        borderRadius: 15,
+        paddingTop: 4,
+        paddingBottom: 4
+    },
 });
